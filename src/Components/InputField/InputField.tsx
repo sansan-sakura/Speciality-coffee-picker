6f49da52d495c4084e6ecc6e12074efc68f47513
Sakura Tanaka <sito6496t@google.com>
2023-10-01 10:51:14 +0200

    styling is much better now

--- src/Components/InputField/InputField.tsx
+++ src/Components/InputField/InputField.tsx
@@ -1,5 +1,5 @@
 import { Form, NextFormat } from "../../types/formType";
-
+import styles from "./InputField.module.css";
 export const InputField = ({
   nextFormat,
   onUserInput,
@@ -20,39 +20,50 @@ export const InputField = ({
 
   if (inputType === "email" || inputType === "text") {
     return (
-      <input type={inputType} onChange={handleInput} placeholder={`Enter your ${fieldName}`} />
+      <input
+        type={inputType}
+        onChange={handleInput}
+        placeholder={`Enter your ${fieldName}`}
+        className={styles.input_text}
+      />
     );
   } else if (inputType === "range") {
     const { min, max } = answerChoice;
     return (
-      <>
-        <span>{answerChoice.min}</span>
+      <div className={styles.range_box}>
         <input
           type={inputType}
           min={min}
           max={max}
           onChange={handleInput}
+          className={styles.input_range}
           value={value.howOften}
         />{" "}
-        <span>{max}</span>
-      </>
+        <p>{value.howOften}</p>
+      </div>
     );
   } else if (inputType === "radio") {
     return (
-      <>
+      <div className={`${styles.radio_box} ${fieldName === "roast" ? styles.roast : ""}`}>
         {answers?.map((el) => (
           <div key={el}>
             <input type="radio" name={fieldName} value={el} onChange={handleInput} id={el} />
             <label htmlFor={el}>{el}</label>
           </div>
         ))}
-      </>
+      </div>
     );
   } else if (inputType === "select") {
-    const valueField = fieldName === "region" ? "region" : "flavor";
+    const valueField = fieldName === "region" ? "region" : "method";
     return (
       <>
-        <select onChange={handleInput} id={fieldName} name={fieldName} value={valueField}>
+        <select
+          onChange={handleInput}
+          id={fieldName}
+          name={fieldName}
+          value={valueField}
+          className={styles.select}
+        >
           {answers?.map((el) => (
             <option value={el} key={el}>
               {el}
